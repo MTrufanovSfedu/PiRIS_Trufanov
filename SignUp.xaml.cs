d9fecadee6e14ed835450dd499b5eaccc9284665
MTrufanovSfedu <mtrufanov@sfedu.ru>
2026-04-06 11:24:38 +0300

    Практическая работа 2: Создание графического интерфейса клиентской части ПО / Технологии реализации клиентского ПО

---
+++ SignUp.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Work3
+{
+    /// <summary>
+    /// Interaction logic for SignUp.xaml
+    /// </summary>
+    public partial class SignUp : Window
+    {
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+
+        private void button_sign_in_Click(object sender, RoutedEventArgs e)
+        {
+            button_sign_in.IsEnabled = false;
+            Login Login = new Login();
+            Login.Visibility = Visibility.Visible;
+            this.Close();
+        }
+    }
+}
